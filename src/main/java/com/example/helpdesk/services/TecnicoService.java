4806785cdb3e69961874ff56901cb11a682bfecc
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-01-31 10:34:18 -0300

    implementar delete para Tecnico

--- src/main/java/com/example/helpdesk/services/TecnicoService.java
+++ src/main/java/com/example/helpdesk/services/TecnicoService.java
@@ -48,6 +48,14 @@ public class TecnicoService {
         return repository.save(oldObj);
     }
 
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+        if(!obj.getChamados().isEmpty()) {
+            throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+        }
+        repository.deleteById(id);
+    }
+    
     private void validaPorCpfEEmail(TecnicoDTO objDTO) {
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
         if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -59,4 +67,5 @@ public class TecnicoService {
             throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
         }       
     }
+
 }
