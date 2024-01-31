382989ca8ed790558068ad6f9448a99ebdece524
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-01-31 09:34:38 -0300

    criar exceção para cpf e email já cadastrados

--- src/main/java/com/example/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/example/helpdesk/repositories/PessoaRepository.java
@@ -3,7 +3,11 @@ package com.example.helpdesk.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.helpdesk.domain.Pessoa;
+import java.util.Optional;
+
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-    
+
+    Optional<Pessoa> findByCpf(String cpf);
+    Optional<Pessoa> findByEmail(String email);
 }
