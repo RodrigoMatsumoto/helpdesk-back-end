0dadbf0a56784139d19947130a3b48d87b0c5045
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-01-31 10:07:57 -0300

    criar validação nos campos de criação de usuário

--- src/main/java/com/example/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.helpdesk.resources.exceptions;
 
+import org.apache.catalina.connector.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,5 +30,17 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, HttpServletRequest request) {
+    
+        ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Validation error", "Erro na validação dos campos", request.getRequestURI());
+
+        for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+            errors.addErrors(x.getField(), x.getDefaultMessage());
+        }
+    
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
