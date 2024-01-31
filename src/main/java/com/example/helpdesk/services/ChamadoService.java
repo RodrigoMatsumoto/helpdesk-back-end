27cc4726c6ec35922a46b3a26ff8509bbb52a1a0
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-01-31 15:09:47 -0300

    criar update para Chamado

--- src/main/java/com/example/helpdesk/services/ChamadoService.java
+++ src/main/java/com/example/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.example.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,6 +40,13 @@ public class ChamadoService {
     public Chamado create(@Valid ChamadoDTO objDTO) {
         return repository.save(newChamado(objDTO));
     }
+    
+    public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
 
     private Chamado newChamado(ChamadoDTO obj) {
         Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -50,6 +58,10 @@ public class ChamadoService {
             chamado.setId(obj.getId());
         }
 
+        if(obj.getStatus().equals(2)) {
+            chamado.setDataFechamento(LocalDate.now());
+        }
+
         chamado.setTecnico(tecnico);
         chamado.setCliente(cliente);
         chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -58,4 +70,5 @@ public class ChamadoService {
         chamado.setObservacoes(obj.getObservacoes());
         return chamado;
     }
+
 }
