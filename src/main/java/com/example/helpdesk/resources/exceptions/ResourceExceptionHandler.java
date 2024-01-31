1fa13eb690b08514a4c2aaab6342e0810b09b87e
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-01-31 12:04:14 -0300

    criar findById para Chamado

--- src/main/java/com/example/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.helpdesk.resources.exceptions;
 
-import org.apache.catalina.connector.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
---
+++ src/main/java/com/example/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.example.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.helpdesk.domain.Chamado;
+import com.example.helpdesk.repositories.ChamadoRepository;
+import com.example.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+    
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}
